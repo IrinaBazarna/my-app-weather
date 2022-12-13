02377ee95523a8ac01d9b0a6372aa6e66e274468
IrinaBazarna <bazarnairen@gmail.com>
2022-12-14 00:08:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Coded by Irina Bazarna
         </a>
       </header>
     </div>
